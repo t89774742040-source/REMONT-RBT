2041701019c0723cb46bd4bf4d0ef0abf860da9d
t89774742040-source <t89774742040@gmail.com>
2026-04-25 18:28:37 +0300

    приложение РБТ

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started: http://localhost:${PORT}`);
+});
